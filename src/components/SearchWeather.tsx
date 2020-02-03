98a2e1a8d0220172e925a63e97245c8f0263dce3
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-02-03 04:56:07 +0100

    Create interface searchWeather

--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
-class searchWeather extends Component {
+interface searchWeatherState {
+  city?: string;
+  temperatureDegree?: number;
+  temperatureMax?: number;
+  temperatureMin?: number;
+  temperatureSensation?: number;
+  weatherType?: string;
+  windSpeed?: number;
+  time?: string;
+}
+
+class searchWeather extends Component<searchWeatherState> {
   weatherRef: React.RefObject<HTMLInputElement>;
 
   constructor(props: any) {
     super(props);
+    this.state = {
+      city: "",
+      temperatureDegree: null,
+      temperatureMax: null,
+      temperatureMin: null,
+      temperatureSensation: null,
+      weatherType: null,
+      windSpeed: null,
+      time: null,
+      error: false
+    };
     this.weatherRef = React.createRef();
   }
 
