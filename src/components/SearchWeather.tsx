0a7f113be68f1c1ce8f133da4cae4a93d2baae0e
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-02-03 04:54:58 +0100

    Create input with ref

--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 class searchWeather extends Component {
+  weatherRef: React.RefObject<HTMLInputElement>;
+
+  constructor(props: any) {
+    super(props);
+    this.weatherRef = React.createRef();
+  }
+
   render() {
     return (
       <div>
-        <input type="text"></input>
+        <input type="text" ref={this.weatherRef}></input>
         <button> Search </button>
       </div>
     );
